6b6d98febe3fc76f5b3ed4b0bcaf445474150413
Timothy Mannon <timothy.mannon@powerflex.com>
2025-03-04 14:38:40 -0800

    draft ability to cycle through sites

--- main.py
+++ main.py
@@ -1,50 +1,21 @@
+import time
+
 from unifi_api_connector import run_unifi_api
+import json
+import time
+
+
+def load_config(filename):
+    with open(filename, 'r') as json_file:
+        data = json.load(json_file)
+    return data
 
 
 if __name__ == '__main__':
-    service_dict = {
-        "site_id_update": False,
-        "site_client_data": {
-            "get_client_data": True,
-            "acn": "0021",
-            "acc": "03",
-            "metrics": [
-                "ap_mac",
-                "oui",
-                "last_ip",
-                "first_seen",
-                "last_seen",
-                "disconnect_timestamp",
-                "last_uplink_mac",
-                "mac",
-                "hostname",
-                "_uptime_by_uap",
-                "_last_seen_by_uap",
-                "ip",
-                "channel",
-                "essid",
-                "noise",
-                "nss",
-                "rx_rate",
-                "rssi",
-                "signal",
-                "tx_mcs",
-                "tx_power",
-                "tx_rate",
-                "tx_retry_burst_count",
-                "uptime",
-                "tx_bytes",
-                "rx_bytes",
-                "tx_packets",
-                "rx_packets",
-                "bytes-r",
-                "tx_bytes-r",
-                "rx_bytes-r",
-                "tx_retries",
-                "wifi_tx_attempts",
-                "wifi_tx_dropped",
-                "wifi_tx_retries_percentage"
-            ]
-        }
-    }
-    run_unifi_api(service_dict)
+    config_files = ['config.json', 'config2.json']
+    keep_on = True
+    while keep_on:
+        for setting in config_files:
+            service_dict = load_config(setting)
+            run_unifi_api(service_dict)
+        time.sleep(600)
