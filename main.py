750d1a9bf13b5bd52fada0907add21b4abae9ff9
Timothy Mannon <timothy.mannon@powerflex.com>
2025-03-04 09:39:59 -0800

    obtain site list

--- main.py
+++ main.py
@@ -2,4 +2,7 @@ from unifi_api_connector import run_unifi_api
 
 
 if __name__ == '__main__':
-    run_unifi_api()
+    service_dict = {
+        "site_id_update": True
+    }
+    run_unifi_api(service_dict)
--- unifi_api_connector.py
+++ unifi_api_connector.py
@@ -41,7 +41,17 @@ def unifi_data(base, path, sess):
         print(f"An error occurred while accessing the API: {e}")
 
 
-def run_unifi_api():
+def pull_site_ids(site_info_dict):
+    site_names = {}
+    for site in site_info_dict["data"]:
+        site_names[site["desc"]] = site["name"]
+    filename = 'unifi_site_list.json'
+    with open(filename, 'w') as json_file:
+        json.dump(site_names, json_file, indent=4)
+    return
+
+
+def run_unifi_api(service):
     load_dotenv('secrets.env')
     base_url = 'https://unifi.prd.powerflex.io:443'
     credentials = {
@@ -53,6 +63,11 @@ def run_unifi_api():
     session = login_to_unifi(base_url, credentials)
 
     if session:
-        metrics = unifi_data(base_url, '/api/s/jiac21ob/stat/sta', session)
-        print(json.dumps(metrics, indent=4))
+        if service['site_id_update']:
+            sites = unifi_data(base_url, '/api/self/sites', session)
+            pull_site_ids(sites)
+            print(json.dumps(sites, indent=4))
+
+        # metrics = unifi_data(base_url, '/api/s/jiac21ob/stat/sta', session)
+        # print(json.dumps(metrics, indent=4))
         logout_of_unifi(base_url, '/api/logout', session)
